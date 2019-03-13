573072d772bd41ecda06569d7f2db9ed4b7ee128
watslink <seb.decroix@gmail.com>
2019-03-13 15:34:03 +0100

    suppression de DatabasePrpoerties et ajout configuration database dans ConfigurationDAO

--- escalade-consumer/src/main/java/com/sd/oc/DAO/ConfigurationDAO.java
+++ escalade-consumer/src/main/java/com/sd/oc/DAO/ConfigurationDAO.java
@@ -1,12 +1,51 @@
 package com.sd.oc.DAO;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath=database.properties")
 public class ConfigurationDAO {
 
+    @Value("${datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(final DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
 }
