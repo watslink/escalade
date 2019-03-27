5ec8135c249900e6cf4411a07181eb21154fe67a
watslink <seb.decroix@gmail.com>
2019-03-27 15:07:51 +0100

    ajout du bean  ViewResolver dans ConfigurationWebApp et modification mapping web.xml

--- escalade-webapp/src/main/java/com/sd/oc/webapp/ConfigurationWebApp.java
+++ escalade-webapp/src/main/java/com/sd/oc/webapp/ConfigurationWebApp.java
@@ -1,11 +1,24 @@
 package com.sd.oc.webapp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.sd.oc.webapp")
 public class ConfigurationWebApp {
+
+    @Bean
+    public UrlBasedViewResolver viewResolver() {
+        UrlBasedViewResolver resolver
+                = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
 }
