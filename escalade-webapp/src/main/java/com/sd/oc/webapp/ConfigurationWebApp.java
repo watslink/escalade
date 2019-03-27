6e9919774290e4648852a87c47a93d506a63971f
watslink <seb.decroix@gmail.com>
2019-03-27 15:03:28 +0100

    creation classe ConfigurationWebApp dans escaladewebapp pour la configuration Spring MVC

---
+++ escalade-webapp/src/main/java/com/sd/oc/webapp/ConfigurationWebApp.java
@@ -0,0 +1,11 @@
+package com.sd.oc.webapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.sd.oc.webapp")
+public class ConfigurationWebApp {
+}
