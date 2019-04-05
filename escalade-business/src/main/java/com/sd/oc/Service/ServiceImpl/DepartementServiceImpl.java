78f773107afea3cffe4b88fa50309631f3c437ec
watslink <seb.decroix@gmail.com>
2019-04-05 15:02:48 +0200

    ajout annotation Spring dans Service et DAO Departement

--- escalade-business/src/main/java/com/sd/oc/Service/ServiceImpl/DepartementServiceImpl.java
+++ escalade-business/src/main/java/com/sd/oc/Service/ServiceImpl/DepartementServiceImpl.java
@@ -4,7 +4,9 @@ import com.sd.oc.DAO.DAOInterface.DepartementDAO;
 import com.sd.oc.Service.ServiceInterface.DepartementService;
 import com.sd.oc.model.Departement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DepartementServiceImpl extends GenericServiceImpl<Departement, String> implements DepartementService {
 
     @Autowired
--- escalade-consumer/src/main/java/com/sd/oc/DAO/DAOImpl/DepartementDAOImpl.java
+++ escalade-consumer/src/main/java/com/sd/oc/DAO/DAOImpl/DepartementDAOImpl.java
@@ -2,6 +2,8 @@ package com.sd.oc.DAO.DAOImpl;
 
 import com.sd.oc.DAO.DAOInterface.DepartementDAO;
 import com.sd.oc.model.Departement;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DepartementDAOImpl extends GenericDAOImpl<Departement, String> implements DepartementDAO {
 }
