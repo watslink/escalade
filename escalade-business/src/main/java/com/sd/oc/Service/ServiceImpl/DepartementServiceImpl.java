379164c20b4ff96c7875b26f5cb357b4c473190d
watslink <seb.decroix@gmail.com>
2019-04-05 15:01:29 +0200

    creation de DepartementService et DepartementServiceImpl

---
+++ escalade-business/src/main/java/com/sd/oc/Service/ServiceImpl/DepartementServiceImpl.java
@@ -0,0 +1,15 @@
+package com.sd.oc.Service.ServiceImpl;
+
+import com.sd.oc.DAO.DAOInterface.DepartementDAO;
+import com.sd.oc.Service.ServiceInterface.DepartementService;
+import com.sd.oc.model.Departement;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DepartementServiceImpl extends GenericServiceImpl<Departement, String> implements DepartementService {
+
+    @Autowired
+    public DepartementServiceImpl(DepartementDAO departementDAO) {
+        this.genericDAO = departementDAO;
+    }
+}
+
---
+++ escalade-business/src/main/java/com/sd/oc/Service/ServiceInterface/DepartementService.java
@@ -0,0 +1,6 @@
+package com.sd.oc.Service.ServiceInterface;
+
+import com.sd.oc.model.Departement;
+
+public interface DepartementService extends GenericService<Departement, String> {
+}
